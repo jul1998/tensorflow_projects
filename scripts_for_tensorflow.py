062cafe31e79968ec89ad3caad1f485d45b85870
Julián Andrés <109244495+jul1998@users.noreply.github.com>
2024-05-08 18:58:44 -0600

    Update scripts_for_tensorflow.py

--- scripts_for_tensorflow.py
+++ scripts_for_tensorflow.py
@@ -280,7 +280,6 @@ def check_valid_images(base_path):
     for root, dirs, files in os.walk(base_path):
         for file in files:
             file_path = os.path.join(root, file)
-            print(file_path)
             try:
                 with Image.open(file_path) as img:
                     img.verify()  # verify that it is, in fact, an image
